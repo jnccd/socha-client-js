15cc9296f08b6c6a3752c70165f1712275b98efd
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-01-19 00:54:55 +0100

    Replace tab

--- logic.js
+++ logic.js
@@ -2,5 +2,5 @@ import { Point, getPossibleMoves } from './main.js'
 
 export function decideMove(board, turn, currentPlayer) {
     let moves = getPossibleMoves(turn, board)
-	return moves[Math.floor(Math.random() * moves.length)]
+    return moves[Math.floor(Math.random() * moves.length)]
 }
